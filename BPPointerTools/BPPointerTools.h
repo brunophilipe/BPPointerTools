89a57a0a4ee0e342a485c7cb28458914bc76dee7
Bruno Philipe <git@bruno.ph>
2020-05-13 20:36:02 +0200

    Initial Commit

---
+++ BPPointerTools/BPPointerTools.h
@@ -0,0 +1,19 @@
+//
+//  BPPointerTools.h
+//  BPPointerTools
+//
+//  Created by Bruno Philipe on 13.05.20.
+//  Copyright © 2020 Bruno Philipe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BPPointerTools.
+FOUNDATION_EXPORT double BPPointerToolsVersionNumber;
+
+//! Project version string for BPPointerTools.
+FOUNDATION_EXPORT const unsigned char BPPointerToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BPPointerTools/PublicHeader.h>
+
+
